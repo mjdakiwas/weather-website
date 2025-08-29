a3a0e73d818126c62f5648b0e549e4f450cf965c
Marlette Dakiwas <mjessdakiwas@gmail.com>
2025-08-29 16:44:06 -0400

    Added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,11 @@
 const request = require("postman-request");
 
+// Goal: Add new data to forecast
+// 1. Update the forecast string to include new data
+// 2. Commit your changes
+// 3. Push your changes to GitHub and deploy to Heroku
+// 4. Test your work in the live applications
+
 const forecast = (latitude, longitude, callback) => {
   const url = `https://api.weatherstack.com/current?access_key=d79e90cb5e3a88949908e6c0b524281e&query=${latitude},${longitude}&units=f`;
 
@@ -11,7 +17,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}.`
       );
     }
   });
